4c0f133dd4220aa4551ccdb977e3c949347113da
Lembek <lembek0076@gmail.com>
2023-12-23 00:47:16 +0300

    init

---
+++ hw1/task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   printf("Hello World!\n");
+   printf("Let's\n");
+   printf("\tgo\n");
+   printf("\t\tto walk\n");
+   printf("   *\n");
+   printf("  ***\n");
+   printf(" *****\n");
+   printf("*******\n");
+   printf(" HH HH\n");
+   printf(" ZZZZZ");
+   return 0;
+}
