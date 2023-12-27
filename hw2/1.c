41441b9e1742401d9947bb8e33e7fdd9d9f7d118
Lembek <lembek0076@gmail.com>
2023-12-27 20:14:45 +0300

    hw2

---
+++ hw2/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Перевести из 10 в 16 систему 12345678, 1000000
+*/
+
+int main(void) {
+    int first = 12345678;
+    int second = 1000000;
+    printf("%x\n", first);
+    printf("%x", second);
+    return 0;
+}
---
+++ hw2/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Перевести из 16 в 10 систему 12345678, 1000000 
+*/
+
+int main(void) {
+    int first = 0x12345678;
+    int second = 0x1000000;
+    printf("%d\n", first);
+    printf("%d", second);
+    return 0;
+}
